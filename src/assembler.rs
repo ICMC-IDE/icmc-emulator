c4af6561ac72d352f3bc2b8ef541dd666ba9cdd7
ViniciusGiroto <viniciusgiroto@outlook.com>
2024-05-22 10:18:12 -0300

    I don't remember

--- src/assembler.rs
+++ src/assembler.rs
@@ -2,87 +2,72 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 struct Code {
-    report: customasm::diagn::Report,
-    fileserver: customasm::util::FileServerMock,
-    opts: customasm::asm::AssemblyOptions,
     result: customasm::asm::AssemblyResult,
 }
 
 #[wasm_bindgen]
-impl Code {
+struct Assembler {
+    report: customasm::diagn::Report,
+    fileserver: customasm::util::FileServerMock,
+}
+
+#[wasm_bindgen]
+impl Assembler {
     #[wasm_bindgen(constructor)]
-    pub fn new(src: &str) -> Self {
-        let virtual_filename = "program.asm";
-
-        let mut report = customasm::diagn::Report::new();
-        let mut fileserver = customasm::util::FileServerMock::new();
-        fileserver.add("icmc.asm", include_str!("./icmc.asm"));
-        fileserver.add("giroto.asm", include_str!("./giroto.asm"));
-        fileserver.add(virtual_filename, src);
-
-        let opts = customasm::asm::AssemblyOptions::new();
-
-        let result = customasm::asm::assemble(
-            &mut report,
-            &opts,
-            &mut fileserver,
-            &["icmc.asm", virtual_filename],
-        );
-
-        Code {
-            report,
-            fileserver,
-            opts,
-            result,
+    pub fn new() -> Self {
+        Self {
+            report: customasm::diagn::Report::new(),
+            fileserver: customasm::util::FileServerMock::new(),
         }
     }
 
-    pub fn symbols(&self) -> Result<String, String> {
-        if self.result.error {
+    pub fn add_std_file(&mut self, filename: &str, contents: &str) {
+        self.fileserver.add_std_files(&[(filename, contents)]);
+    }
+
+    pub fn add_file(&mut self, filename: &str, contents: &str) {
+        self.fileserver.add(filename, contents);
+    }
+
+    pub fn assemble(&mut self, filenames: &str) -> Result<Code, String> {
+        let opts = customasm::asm::AssemblyOptions::new();
+        let filenames = filenames.split(':').collect::<Vec<_>>();
+
+        let result =
+            customasm::asm::assemble(&mut self.report, &opts, &mut self.fileserver, &filenames);
+
+        if result.error {
             let mut vec = Vec::new();
             self.report.print_all(&mut vec, &self.fileserver, true);
             return Err(String::from_utf8(vec).unwrap());
         }
 
+        Ok(Code { result })
+    }
+}
+
+#[wasm_bindgen]
+impl Code {
+    pub fn symbols(&self) -> String {
         let decls = self.result.decls.as_ref().unwrap();
         let defs = self.result.defs.as_ref().unwrap();
 
-        Ok(decls.symbols.format_default(decls, defs))
+        decls.symbols.format_default(decls, defs)
     }
 
-    pub fn binary(&self) -> Result<Vec<u16>, String> {
-        if self.result.error {
-            let mut vec = Vec::new();
-            self.report.print_all(&mut vec, &self.fileserver, true);
-            return Err(String::from_utf8(vec).unwrap());
-        }
-
-        let Some(ref output) = self.result.output else {
-            unreachable!()
-        };
-
-        Ok(output
+    pub fn binary(&self) -> Vec<u16> {
+        self.result
+            .output
+            .as_ref()
+            .unwrap()
             .format_binary()
             .into_iter()
             .array_chunks()
             .map(|bytes| u16::from_be_bytes(bytes))
-            .collect())
+            .collect()
     }
 
-    pub fn mif(&self) -> Result<String, String> {
-        if self.result.error {
-            let mut vec = Vec::new();
-            self.report.print_all(&mut vec, &self.fileserver, true);
-            return Err(String::from_utf8(vec).unwrap());
-        }
-
-        let decls = self.result.decls.as_ref().unwrap();
-        let defs = self.result.defs.as_ref().unwrap();
-
-        let Some(ref output) = self.result.output else {
-            unreachable!()
-        };
-
-        Ok(output.format_mif())
+    pub fn mif(&self) -> String {
+        self.result.output.as_ref().unwrap().format_mif()
     }
 }
